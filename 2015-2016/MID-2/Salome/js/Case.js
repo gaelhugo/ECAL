fe3c19935aa9501bb8cc6a7314de92a9e13b909f
gaelhugo <gael.hugo@onemore-studio.com>
2015-11-27 23:28:21 +0100

    Update Salome's circle
    Add interaction

--- 2015-2016/MID-2/Salome/js/Case.js
+++ 2015-2016/MID-2/Salome/js/Case.js
@@ -1,14 +1,19 @@
-var Case = function(x,y,radiusMax,angle,angleDepart,ctx,id){
+var Case = function(x,y,radiusMax,angle,angleDepart,ctx,id,caseWidth){
 	this.x = x;
 	this.y = y;
 	this.radius= radiusMax;
 	this.angle = angle;
 	this.angleDepart = angleDepart*Math.PI/180;
 	this.ctx = ctx;
-
-	this.color = 'rgb('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+')';
+	this.caseWidth = caseWidth;
+	this.color = this.originalColor = 'rgb('+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+','+Math.round(Math.random()*255)+')';
 	this.strokeColor = '#8ec448';
 	this.ID = id;
+	//DE QUEL GRAND CERCLE CETTE CASE FAIT PARTIE
+	this.areaIndex = (this.radius / this.caseWidth)-1;
+	//rotation in RAD
+	this.rotation = {"x":0 * Math.PI/180};
+
 }
 
 Case.prototype= {
@@ -18,13 +23,58 @@ Case.prototype= {
 		this.ctx.fillStyle = this.color;
 		this.ctx.strokeStyle = this.strokeColor;
 		this.ctx.moveTo(this.x,this.y);
-		this.ctx.lineTo( Math.cos(this.angleDepart)*this.radius + this.x, Math.sin(this.angleDepart)*this.radius +this.y);
-		this.ctx.arc(this.x,this.y,this.radius,this.angleDepart,this.angle+this.angleDepart,false);
+		this.ctx.lineTo( Math.cos(this.angleDepart + this.rotation.x)*this.radius + this.x, Math.sin(this.angleDepart+this.rotation.x)*this.radius +this.y);
+		this.ctx.arc(this.x,this.y,this.radius,this.angleDepart+this.rotation.x,this.angle+this.angleDepart+this.rotation.x,false);
 		this.ctx.lineTo(this.x, this.y);
-		
-		
 		this.ctx.fill();
 		this.ctx.stroke();
 		this.ctx.closePath();
+	},
+
+	check:function(x,y){
+		var angle = this.getAngle(this.x,this.y,x,y)*180/Math.PI;
+			angle = (angle<0)?360+angle:angle;
+			angle = angle - this.rotation.x*180/Math.PI;
+			if(angle<0){
+				angle+=360;
+				//this.rotation.x-=360*Math.PI/180; 
+			}
+		var dist = this.getDistance(this.x,this.y,x,y);
+		if(angle>=this.angleDepart*180/Math.PI && angle<=this.angleDepart*180/Math.PI+this.angle*180/Math.PI && dist<this.radius && dist>(this.radius-this.caseWidth)){
+			this.color = '#ffffff';
+			return {"id":this.ID,"radius":this.radius};
+		}else{
+			this.color = this.originalColor;
+		}
+	},
+	/*
+		Récupérer la distance entre 2 points
+	*/
+	getDistance:function(x1,y1,x2,y2){
+		return Math.sqrt( (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) );
+	},
+	/* 
+		Récupérer l'angle entre 2 points en radian
+	*/
+	getAngle:function(x1,y1,x2,y2){
+		return Math.atan2(y2 - y1, x2 - x1);
+	},
+	tween:function(val){
+		// IL FAUT CHECKER LA ROTATION ICI CAR ELLE PEUT DEVENIR TRES GRANDE SI ON NE LUI CLICK JAMAIS DESSUS
+		var finalRotation = val*Math.PI/180+this.rotation.x;
+		/* good mais reviens en arrière
+		if(finalRotation>=360*Math.PI/180){
+			finalRotation = 0;
+		}
+		*/
+		var tween = new TWEEN.Tween(this.rotation)
+		.to({x:finalRotation},1000)
+		.easing(TWEEN.Easing.Elastic.Out)
+		.onComplete((function(){
+			if(this.rotation.x>=360*Math.PI/180){
+				this.rotation.x = 0;
+			}
+		}).bind(this))
+		.start();
 	}
 }
--- 2015-2016/MID-2/Salome/js/app.js
+++ 2015-2016/MID-2/Salome/js/app.js
@@ -10,40 +10,60 @@ var angle 		= 30*Math.PI/180;
 var caseWidth 	= 70;
 var nbrCase		= 5;
 var allCases 	= [];
-var centre 		 = {"x":window.innerWidth/2,"y":window.innerHeight/2};
+var centre 		= {"x":window.innerWidth/2,"y":window.innerHeight/2};
 
 function setup(){
 
 	//creation de toutes les Case
 	var id = 0;
-	for(var i =0;i<360;i+=30){
-
-		//pour avoir les 5 cases par ANGLE
-		var radiusMax = nbrCase*caseWidth;
-		while(radiusMax>0){
+	//pour avoir les 5 cases par ANGLE
+	var radiusMax = nbrCase*caseWidth;
+	while(radiusMax>0){
+		for(var i =0;i<360;i+=30){
 			//creation de case
-			var maCase = new Case(centre.x,centre.y,radiusMax,angle,i,ctx,id);
+			var maCase = new Case(centre.x,centre.y,radiusMax,angle,i,ctx,id,caseWidth);
 			allCases.push(maCase);
-			radiusMax-=caseWidth;
 			id++;
 		}
-
+		radiusMax-=caseWidth;
 	}
-
-
+	document.addEventListener("mousemove", onMouseMove);
+	document.addEventListener("click", onMouseClick);
 	draw();
 }
 
+function onMouseMove(e){
+	//JUST FOR ROLLOVER
+	for(var i=0;i<allCases.length;i++){
+		allCases[i].check(e.pageX,e.pageY);
+	}
+}
 
+function onMouseClick(e){
+	var myCase;
+	for(var i=0;i<allCases.length;i++){
+		myCase = allCases[i].check(e.pageX,e.pageY);
+		if(myCase!=undefined){
+			break;
+		}	
+	}
+	//TWEEN JUST THE SELECTED CIRCLE
+	if(myCase!=undefined){
+		for(var i=0;i<allCases.length;i++){
+			if(allCases[i].radius == myCase.radius){
+				allCases[i].tween(30);
+			}
+		}
+	}
+}
 
 function draw(){
+	TWEEN.update();
 	ctx.clearRect(0,0,w,h);
-
 	//dessiner
 	for(var i=0;i<allCases.length;i++){
 		allCases[i].display();
 	}
-
 	requestAnimationFrame(draw);
 }
 setup();
